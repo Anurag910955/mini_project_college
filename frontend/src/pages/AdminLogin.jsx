fe406986cdf8bed8c4efcc378ec476b116fa4bc0
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-04-09 23:12:00 +0530

    Update AdminLogin.jsx

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -12,7 +12,7 @@ const AdminLogin = () => {
     setError('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/admin/login', {
+      const res = await fetch('https://mini-project-college.onrender.com/api/admin/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
