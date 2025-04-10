06c28e2a0bfe326ca359bf834341c1f0e1be21b9
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-04-10 23:59:51 +0530

    final

--- frontend/src/pages/Booking.jsx
+++ frontend/src/pages/Booking.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import QRCode from "qrcode.react"; // You'll need to install this package: npm install qrcode.react
+import { QRCode } from "qrcode.react";
+
 
 const Booking = () => {
   const { id } = useParams();
