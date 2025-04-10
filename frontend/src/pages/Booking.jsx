d9dd03c41857d0eddb000583deeeccd1611064cd
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-04-11 01:21:30 +0530

    i

--- frontend/src/pages/Booking.jsx
+++ frontend/src/pages/Booking.jsx
@@ -146,7 +146,7 @@ const Booking = () => {
               value={formData.tickets}
               onChange={handleChange}
               required
-              min="1"
+              min=""
               className="w-full px-4 py-3 border text-gray-700 border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
             />
           </div>
